516a449f4cc45da94221c2ee9556408fa3bf3b36
solomon-group3 <157471270+solomon-group3@users.noreply.github.com>
2025-10-08 01:42:48 +0800

    Data List structure comments and notes

--- src/Student/Portal/Main.java
+++ src/Student/Portal/Main.java
@@ -1,4 +1,35 @@
 package Student.Portal;
+import java.util.*;
 
 public class Main {
+    static Scanner input = new Scanner(System.in);
+    private static DoublyLinkedList<DoublyLinkedList<DoublyLinkedList<Student>>> portal = new DoublyLinkedList<>();
+
+    public static void main(String[] args) {
+        /*
+        NOTE
+            - program will be a 4 level list
+            - sublist of schools will be fixed, CRUD will only be allowed in program, course, and student levels
+            - inaantok n ko bukas na ulit
+
+        UNIVERSITY (main list)
+            - SAMCIS (sublist 1)
+                - BSCS (Program 1)
+                    - Data Structures (Course 1)
+                        - Student 1
+                        - Student 2
+                        - Student 3
+                        - Student 4
+                    - Operating Systems (Course 2)
+                    - Human Computer Interaction (Course 3)
+                - BSIT (Program 2)
+                - BMMA (Program 3)
+                - BSA (Program 4)
+            - SEA (sublist 1)
+            - SOHNABS (sublist 2)
+            - STELLA (sublist 3)
+            - SOM (sublist 4)
+            -
+         */
+    }
 }
